394b00d84875dfb3116747a871aec1ecc3ef4f12
Tom Maiaroto <tom.maiaroto@gmail.com>
2019-02-04 10:47:57 -0800

    remove test keys

--- App/config.js
+++ App/config.js
@@ -1,6 +1,6 @@
 const config = {
-    APCA_API_KEY_ID: '***REMOVED***',
-    APCA_API_SECRET_KEY: '***REMOVED***',
+    APCA_API_KEY_ID: 'key_id',
+    APCA_API_SECRET_KEY: 'secret_key',
     // BASE_URL: 'https://api.alpaca.markets/',
     BASE_URL: 'https://paper-api.alpaca.markets/',
     DATA_BASE_URL: 'https://data.alpaca.markets/'
